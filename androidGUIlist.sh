6609fb618a43b06363c9259a02886220487e0f36
Bogdanov64 <91365303+Bogdanov64@users.noreply.github.com>
2025-02-21 16:20:02 +0200

    Update androidGUIlist.sh

--- androidGUIlist.sh
+++ androidGUIlist.sh
@@ -1,33 +1,41 @@
-                                                                                                               
 #!/bin/bash
 
+# Define the options
 options=("xfce4" "fvwm" "openbox" "fluxbox")
 
-# Print the menu and store the selected option in the variable 'selected'
-select selected in "${options[@]}"; do
-  case $selected in
-    "xfce4")
-      command="dbus-launch --exit-with-session xfce4-session"
-      break
-      ;;
-    "fvwm")
-      command="dbus-launch --exit-with-session fvwm"
-      break
-      ;;
-    "openbox")
-      command="dbus-launch --exit-with-session openbox"
-      break
-      ;;
-    "fluxbox")
-      command="dbus-launch --exit-with-session fluxbox"
-      break
-      ;;
-    *) echo "Invalid option";;
-  esac
-done
+# Create a dialog menu
+selected=$(dialog --title "Select a Window Manager" \
+                  --menu "Choose one of the following options:" \
+                  15 50 4 \
+                  "${options[@]}" \
+                  3>&1 1>&2 2>&3)
+
+# Check if the user pressed Cancel
+exit_status=$?
+if [ $exit_status != 0 ]; then
+  echo "User canceled the selection."
+  exit 1
+fi
+
+# Determine the command based on the selected option
+case $selected in
+  "xfce4")
+    command="dbus-launch --exit-with-session xfce4-session"
+    ;;
+  "fvwm")
+    command="dbus-launch --exit-with-session fvwm"
+    ;;
+  "openbox")
+    command="dbus-launch --exit-with-session openbox"
+    ;;
+  "fluxbox")
+    command="dbus-launch --exit-with-session fluxbox"
+    ;;
+  *) 
+    echo "Invalid option"
+    exit 1
+    ;;
+esac
 
 # Execute the selected command with 'termux-x11'
 termux-x11 :1 -xstartup "$command"
-
-
-
